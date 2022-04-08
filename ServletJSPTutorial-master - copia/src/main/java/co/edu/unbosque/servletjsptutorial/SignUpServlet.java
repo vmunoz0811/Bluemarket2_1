6effe1bd70d001e07f2ff78d5c83f5aaa4e97d0e
mencisom@unbosque.edu.co <100002319+Mencisom@users.noreply.github.com>
2022-04-07 23:49:43 -0500

    frontend, login and signup

---
+++ ServletJSPTutorial-master - copia/src/main/java/co/edu/unbosque/servletjsptutorial/SignUpServlet.java
@@ -0,0 +1,32 @@
+package co.edu.unbosque.servletjsptutorial;
+
+import co.edu.unbosque.servletjsptutorial.services.UserService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+
+@WebServlet(name = "signup", value = "/signup")
+public class SignUpServlet extends HttpServlet {
+
+    public void init() {}
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            new UserService().createUser(username, password, getServletContext().getRealPath("") + File.separator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("./index2.html");
+    }
+
+    public void destroy() {}
+}
